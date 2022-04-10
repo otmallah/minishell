4d9a62055ed34bc86460ef736c85b84ab4208852
othmanmallah <88326652+otmallah@users.noreply.github.com>
2022-04-10 06:07:05 +0000

    >> done

--- pipe2.c
+++ pipe2.c
@@ -14,8 +14,6 @@ int	find_space2(char *tab)
 	return 0;
 }
 
-void	check_cmd_if_built_func(t_mini *index, t_idx *id, char *str);
-
 void	find_path(char *str, t_pipe *index, t_mini *id, t_idx *idx)
 {
 	int i;
@@ -27,7 +25,7 @@ void	find_path(char *str, t_pipe *index, t_mini *id, t_idx *idx)
 	temp = getenv("PATH");
 	index->tab = ft_split(temp, ':');
 	int res = find_space2(str);
-	check_cmd_if_built_func(id, idx, str);
+	//check_cmd_if_built_func(id, idx, str);
 	if (res == 1)
 	{
 		tep = ft_split(str, ' ');
@@ -52,7 +50,7 @@ void	find_path(char *str, t_pipe *index, t_mini *id, t_idx *idx)
 
 }
 
-int	find_len4(char **tab)
+int	find_len5(char **tab)
 {
 	int i;
 
@@ -62,27 +60,49 @@ int	find_len4(char **tab)
 	return i;
 }
 
-void	check_cmd_if_built_func(t_mini *index, t_idx *id, char *str)
-{
-	int a;
-	char **tab;
+// void	check_cmd_if_built_func(t_mini *index, t_idx *id, char *str)
+// {
+// 	int a;
+// 	char **tab;
 
-	a = find_space2(str);
-	if (a == 1)
+// 	a = find_space2(str);
+// 	if (a == 1)
+// 	{
+// 		tab = ft_split(str, ' ');
+// 	}
+// 	else
+// 	{
+// 		tab = (char **)malloc(sizeof(char *));
+// 		tab[0] = str;
+// 	}
+// 	if (ft_strcmp(tab[0], "export") == 0)
+// 	{
+// 		if (a == 1)
+// 			ft_export(index, id, tab[1]);
+// 		else
+// 			ft_export(index, id, NULL);
+// 	}
+// }
+
+void	find_heredoc2(t_pipe *pipx, t_mini *index, t_idx *id)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (pipx->tab[i])
 	{
-		tab = ft_split(str, ' ');
-	}
-	else
-	{
-		tab = (char **)malloc(sizeof(char *));
-		tab[0] = str;
-	}
-	if (ft_strcmp(tab[0], "export") == 0)
-	{
-		if (a == 1)
-			ft_export(index, id, tab[1]);
-		else
-			ft_export(index, id, NULL);
+		while (pipx->tab[i][j])
+		{
+			if (pipx->tab[i][j] == '<' && pipx->tab[i][j + 1] == '<')
+			{
+				ft_heredoce(index, id, pipx, pipx->tab[i]);
+			}
+			j++;
+		}
+		j = 0;
+		i++;
 	}
 }
 
@@ -94,13 +114,14 @@ int ft_pipe(t_mini *index, t_pipe *pipx, t_idx *idx)
 	int i;
 	int sp;
 	int id;
+	int pid[100];
 
-	a = find_len4(pipx->tab);
+	find_heredoc2(pipx, index, idx);
+	a = find_len5(pipx->tab);
 	i = 0;
 	int ff = 0;
 	while (i < a)
 	{
-		//puts("*-*-*-*-*-*-*-**-");
 		if (pipe(fd) == -1)
 			return 1;
 		id = fork();
@@ -108,58 +129,83 @@ int ft_pipe(t_mini *index, t_pipe *pipx, t_idx *idx)
 		{
 			if (i == 0)
 			{
-				str = ft_getenv("PATH", index);
-				if (str == NULL)
-				{
-					printf("command not found:%s\n", pipx->tab[i]);
-					break ;
-				}
-				else
-				{
-					close(fd[0]);
-					dup2(0, 0);
-					dup2(fd[1], STDOUT_FILENO);
-					find_path(pipx->tab[i], pipx , index, idx);
-				}
+				close(fd[0]);
+				dup2(0, 0);
+				dup2(fd[1], STDOUT_FILENO);
+				find_path(pipx->tab[i], pipx , index, idx);
 			}
 			else if (i == (a - 1))
 			{
-				str = ft_getenv("PATH", index);
-				if (str == NULL)
-				{
-					printf("command not found:%s\n", pipx->tab[i]);
-				}
-				else
-				{
-					close(fd[0]);
-					close(fd[1]);
-					dup2(ff, STDIN_FILENO);
-					dup2(1, 1);
-					find_path(pipx->tab[i], pipx , index, idx);
-				}
+				close(fd[0]);
+				close(fd[1]);
+				dup2(ff, STDIN_FILENO);
+				dup2(1, 1);
+				find_path(pipx->tab[i], pipx , index, idx);
 			}
 			else
 			{
-				str = ft_getenv("PATH", index);
-				if (str == NULL)
-					printf("command not found:%s\n", pipx->tab[i]);
-				else
-				{
-					close(fd[0]);
-					dup2(ff, STDIN_FILENO);
-					dup2(fd[1], STDOUT_FILENO);
-					find_path(pipx->tab[i], pipx , index, idx);
-				}
+				close(fd[0]);
+				dup2(ff, STDIN_FILENO);
+				dup2(fd[1], STDOUT_FILENO);
+				find_path(pipx->tab[i], pipx , index, idx);
 			}
 		}
+		pid[i] = id;
 		ff = dup(fd[0]);
 		close(fd[0]);
 		close(fd[1]);
-		wait(NULL);
+		// wait(NULL);
 		i++;
 	}
-	//close(fd[1]);
-	//close(fd[0]);
-	//close(fds);
+	while (--i >= 0)
+		waitpid(pid[i], 0, 0);
 	return 0;
+}
+
+int		find_pipe(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '|')
+			return 1;
+		i++;
+	}
+	return 0;
+}
+
+void	check_pipe(t_pipe *pipe, t_mini *index, t_idx *id, char *str)
+{
+	int a;
+
+	a = find_pipe(str);
+	if (a == 1)
+	{
+		pipe->tab = ft_split(str, '|');
+		ft_pipe(index, pipe, id);
+	}
+}
+
+
+int main(int ac, char **av, char **env)
+{
+	t_mini index;
+	t_idx id;
+	t_pipe pipx;
+
+	index.count2 = 0;
+	index.finde = NULL;
+	index.tab_exp = env;
+	index.string = env;
+	index.tab_unset = env;
+	id.c = 0;
+	id.cnt = 0;
+	id.a = 0;
+	id.b = 0;
+	id.d = 0;
+	id.poor = 1;
+	id.cot = 0;
+	check_pipe(&pipx, &index, &id, av[1]);
 }
